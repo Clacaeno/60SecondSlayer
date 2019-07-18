e78cabc40bad9016c712e77058dc4bb0fd8b0d41
GamerBoi93 <slenderheroftw@gmail.com>
2019-07-18 12:13:35 -0700

    Made Things Better I Think
    please dont yell at me

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,9 @@
 #include "Log.hpp"
 #include "Audio.hpp"
 #include "Input.hpp"
-#include "Debug.hpp"
 #include "GameState.hpp"
+#include "Debug.hpp"
 #include <iostream>
-
 
 using Aspen::Engine::Engine;
 using Aspen::GameState::GameState;
@@ -24,99 +23,155 @@ void ChangeScene(Aspen::Graphics::UI::Button *button, std::string scene, GameSta
   gsm->SetCurrentState(scene);
 }
 
-class MainMenu : public GameState
-{ 
-      float charPosX = 505.0f;
-    float charPosY = 1681.0f;
 
-    float enPosX = 2885.0f;
-    float enPosY = 1701.0f;
+    float charPosX = 543.0f;
+    float charPosY = 1684.0f;
+
+    float enPosX = 2948.0f;
+    float enPosY = 1618.0f;
 
     float bgPosX = 490.0f;
     float bgPosY = 360.0f;
 
-    float offScrX = -1436.0f;
-    float offScrY = 566.0f;
+class BackGround : public Object
+{
+    Aspen::Graphics::Animation *BG;
+
+    public:
+    BackGround(Object *parent = nullptr, std::string name = "BG") : Object(parent, name)
+    {
+    BG = new Aspen::Graphics::Animation(new Aspen::Graphics::UniformSpritesheet("./resources/BG.png", 980, 720 , 1, nullptr, "BG"));
+    BG->GetTransform()->SetPosition(bgPosX,bgPosY);
+    BG->GetTransform()->SetScale(1,1);
+    AddChild(BG);
+    }
+
+    void OnUpdate()
+    {
+    }
+};
+class Player : public Object
+{
+    int Hp;
+    int MaxHP;
+    int Speed;
+    int Attack;
+    int Defense;
+    int Gold;
+  Aspen::Graphics::Animation *RogueIdle;
+  Aspen::Graphics::Animation *RogueStab;
+
+    public:
+    Player(Object *parent = nullptr, std::string name = "Player") : Object(parent, name)
+    {
+    RogueIdle = new Aspen::Graphics::Animation(new Aspen::Graphics::UniformSpritesheet("./resources/Rogue.png", 960, 768 , 1, nullptr, "RogueIdle"));
+    RogueStab = new Aspen::Graphics::Animation(new Aspen::Graphics::UniformSpritesheet("./resources/RogueStab.png", 960, 768 , 6, nullptr, "RogueStab"));
+    RogueStab->GetTransform()->SetScale(0.3 , 0.3);
+    RogueIdle->GetTransform()->SetScale(0.3 , 0.3);
+    RogueIdle->GetTransform()->SetPosition(charPosX,charPosY);
+    RogueStab->GetTransform()->SetPosition(charPosX,charPosY);
+    AddChild(RogueStab);
+    AddChild(RogueIdle);
+    RogueStab->Deactivate();
+    }
+
+    void OnUpdate()
+    {
+    RogueStab->Deactivate();
+    RogueIdle->Activate();
+    if(Aspen::Input::KeyHeld(SDLK_SPACE))
+    {
+      RogueIdle->Deactivate();
+      RogueStab->Activate();
+    }
+    }
+};
+class Slop : public Object
+{
+    int Hp;
+    int MaxHP;
+    int Speed;
+    int Attack;
+    int Defense;
+  Aspen::Graphics::Animation *SlopBounce;
+  Aspen::Graphics::Animation *SlopHurt;
+
+    public:
+    Slop(Object *parent = nullptr, std::string name = "Slop") : Object(parent, name)
+    {
+    SlopBounce = new Aspen::Graphics::Animation(new Aspen::Graphics::UniformSpritesheet("./resources/Slop1.png", 1024, 1024 , 5, nullptr, "SlopBounce"));
+    SlopHurt = new Aspen::Graphics::Animation(new Aspen::Graphics::UniformSpritesheet("./resources/Slop2.png", 1024, 1024 , 2, nullptr, "SlopHurt"));
+    SlopBounce->GetTransform()->SetScale(0.3 , 0.3);
+    SlopHurt->GetTransform()->SetScale(0.3 , 0.3);
+    SlopBounce->GetTransform()->SetPosition(2948, 1618);
+    SlopHurt->GetTransform()->SetPosition(2948, 1618);
+    AddChild(SlopHurt);
+    AddChild(SlopBounce);
+    SlopBounce->Activate();
+    SlopHurt->Deactivate();
+    }
+
+
+  void OnUpdate()
+  {
+    SlopBounce->Activate();
+    SlopHurt->Deactivate();
+    if(Aspen::Input::KeyHeld(SDLK_SPACE))
+    {
+      SlopBounce->Deactivate();
+      SlopHurt->Activate();
+    }
+
+  }
+};
+
+class StoreOwner : public Object
+{
+  Aspen::Graphics::Animation *anim;
+
+    public:
+    StoreOwner(Object *parent = nullptr, std::string name = "Store Owner") : Object(parent, name)
+    {
+    anim = new Aspen::Graphics::Animation(new Aspen::Graphics::UniformSpritesheet("./resources/Rogue.png", 960, 768 , 1, nullptr, "RogueIdle"));
+    AddChild(anim);
+    }
+
+    void OnUpdate()
+    {
+    if(Aspen::Input::KeyHeld(SDLK_a))
+    {
+
+    }
+    if(Aspen::Input::KeyHeld(SDLK_s))
+    {
+        
+    }
+    if(Aspen::Input::KeyHeld(SDLK_d))
+    {
+        
+    }
+    }
+};
+
+class MainMenu : public GameState
+{   
+    double time;
     
   Aspen::Graphics::UI::Text *title;
   Aspen::Graphics::Animation *sprite; 
-  Aspen::Graphics::Animation *ss4;
-  Aspen::Graphics::Animation *ss5;
-  Aspen::Graphics::Animation *ss6;
-  Aspen::Graphics::Animation *ss7;
-  Aspen::Graphics::Animation *ss8;
+
 
   public:
   MainMenu(Object *parent = nullptr, std::string name = "Mainmenu") : GameState(parent, name)
   { 
-    Aspen::Log::Debug("Started making main menu");
-
-    //  Aspen::Graphics::UniformSpritesheet *ss2 = new Aspen::Graphics::UniformSpritesheet("./resources/Colosseum.png", 1885, 1414, 1, nullptr, "ColosseumSS");
-    //  sprite = new Aspen::Graphics::Animation(ss2, 1.0f / 12.0f, this, "Colosseum");
-    //  ss2->GetTransform()->SetPosition(529,399);
-    //  ss2->GetTransform()->SetScale(0.590 , 0.590);
-    //  AddChild(sprite);
-
-    //  Aspen::Graphics::UniformSpritesheet *ss3 = new Aspen::Graphics::UniformSpritesheet("./resources/menu.png", 100,100 , 1, nullptr, "MenuSS");
-    //  sprite = new Aspen::Graphics::Animation(ss3, 1.0f / 12.0f, this, "Menu");
-    //  ss3->GetTransform()->SetPosition(529,399);
-    //  ss3->GetTransform()->SetScale(0.590 , 0.590);
-    //  AddChild(sprite);
-
-    ss4 = new Aspen::Graphics::Animation(new Aspen::Graphics::UniformSpritesheet("./resources/Rogue.png", 960, 768 , 1, nullptr, "RogueIdle"));
-    ss5 = new Aspen::Graphics::Animation(new Aspen::Graphics::UniformSpritesheet("./resources/RogueStab.png", 960, 768 , 6, nullptr, "RogueStab"));
-    ss6 = new Aspen::Graphics::Animation(new Aspen::Graphics::UniformSpritesheet("./resources/Slop1.png", 1024, 1024 , 5, nullptr, "SlopBounce"));
-    ss7 = new Aspen::Graphics::Animation(new Aspen::Graphics::UniformSpritesheet("./resources/Slop2.png", 1024, 1024 , 2, nullptr, "SlopHurt"));
-    ss8 = new Aspen::Graphics::Animation(new Aspen::Graphics::UniformSpritesheet("./resources/BG.png", 980, 720 , 1, nullptr, "BG"));
-    //sprite = new Aspen::Graphics::Animation(ss5, 1.0f / 12.0f, this, "RogueStab");
-    
-    ss5->GetTransform()->SetScale(0.3 , 0.3);
-    ss6->GetTransform()->SetScale(0.3 , 0.3);
-    ss7->GetTransform()->SetScale(0.3 , 0.3);
-    ss7->GetTransform()->SetScale(1 , 1);
-
-    //Aspen::Graphics::UniformSpritesheet *ss4 = new Aspen::Graphics::UniformSpritesheet("./resources/Rogue.png", 960, 768 , 1, nullptr, "RogueIdle");
-    //sprite = new Aspen::Graphics::Animation(ss4, 1.0f / 12.0f, this, "RogueIdle");
-    //  ss4->GetTransform()->SetPosition(offScrX,offScrY);
-    ss8->GetTransform()->SetPosition(bgPosX,bgPosY);
-    ss4->GetTransform()->SetScale(0.3 , 0.3);
-    AddChild(ss8);
-    AddChild(ss7);
-    AddChild(ss6);
-    AddChild(ss5);
-    AddChild(ss4);
-    ss5->GetTransform()->SetPosition(offScrX,offScrY);
-    ss6->GetTransform()->SetPosition(enPosX,enPosY);
-    ss7->GetTransform()->SetPosition(offScrX,offScrY);
-
-
-    //  title = new Aspen::Graphics::UI::Text("WEEEEEEEEEEE", this, "Title");
-    //  AddChild(title);
-    //  title->GetTransform()->SetPosition(201, 83);
-    //  title->GetTransform()->SetRotation(0.174f);
-    //  title->GetTransform()->SetScale(1.56, 2.6);
-    //  title->CreateChild<Aspen::Physics::Rigidbody>();
-
-    
-
+    CreateChild<BackGround>();
+    CreateChild<Player>();
+    CreateChild<Slop>();
 
   }
-  
+
   void OnUpdate()
   {
-    ss5->GetTransform()->SetPosition(offScrX,offScrY);
-    ss4->GetTransform()->SetPosition(charPosX,charPosY);
-    ss6->GetTransform()->SetPosition(enPosX,enPosY);
-    ss7->GetTransform()->SetPosition(offScrX,offScrY);
-    if(Aspen::Input::KeyPressed(SDLK_a))
-    {
-      ss4->GetTransform()->SetPosition(offScrX,offScrY);
-      ss5->GetTransform()->SetPosition(charPosX,charPosY);
-      ss6->GetTransform()->SetPosition(offScrX,offScrY);
-      ss7->GetTransform()->SetPosition(enPosX,enPosY);
-
-    }
-
   }
 
 };
